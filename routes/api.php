29a982b9090592d355a91ae0ecd55cf2101e03a7
dendy250712 <dendysatyaturangga@gmail.com>
2023-09-07 16:04:12 +0700

    update api2

--- routes/api.php
+++ routes/api.php
@@ -21,11 +21,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::controller(PostController::class)->group(function(){
+Route::controller(PostController::class)->group(function () {
 
-    Route::get('posts','indexApi');
-    Route::post('posts','storeApi');
-    Route::get('posts/{id}','showApi');
-    Route::patch('posts/{id}','updateApi');
-    Route::delete('posts/{id}','destroyApi');
+    Route::get('posts', 'indexApi');
+    Route::post('posts', 'storeApi');
+    Route::get('posts/{id}', 'showApi');
+    Route::post('posts/{id}', 'updateApi');
+    Route::delete('posts/{id}', 'destroyApi');
 });
